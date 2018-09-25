5ab0010f13baca0536074f2075de606bfd57aa24
Raf <rafajak@gmail.com>
2018-09-25 20:10:20 +0200

    resize

--- 2018_09_25-one-hour/sketch.js
+++ 2018_09_25-one-hour/sketch.js
@@ -14,8 +14,8 @@ function draw() {
 	let angle1 = map(mouseX, 0, windowWidth, 0, 360) + 270;
 	let angle2 = map(mouseX, 0, windowWidth, 0, 30);
 
-	let r1 = 200;
-	let r2 = 100;
+	let r1 = 100;
+	let r2 = 50;
 
 	let x1 = windowWidth / 2;
 	let y1 = windowHeight / 2;
@@ -25,17 +25,17 @@ function draw() {
 
 	let dx2 = windowWidth / 2 + cos(angle2) * r2
 	let dy2 = windowHeight / 2 + sin(angle2) * r2
-
+	strokeWeight(1);
 	point(x1, y1);
 	point(dx1, dy1);
 
 	point(dx2, dy2);
+	strokeWeight(6);
 
 	line(x1, y1, dx1, dy1);
 	line(x1, y1, dx2, dy2);
 
 	noFill();
-	strokeWeight(5)
-	stroke(map(mouseX, 0, windowWidth, 0, 255));
-	ellipse(width / 2, height / 2, 410);
+	stroke(map(mouseX, 0, windowWidth, 0, 200));
+	ellipse(windowWidth / 2, windowHeight / 2, 200);
 }
