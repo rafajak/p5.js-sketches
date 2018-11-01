716a389a83b6ee8f9725516a0adf0bda02caea86
Raf <rafajak@users.noreply.github.com>
2018-11-01 18:34:07 +0100

    Update sketch.js

--- 2018_11_01-Bambetle/sketch.js
+++ 2018_11_01-Bambetle/sketch.js
@@ -1,22 +1,22 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-	background(0);
+function draw() {	
+  background(0);
+	stroke(127,255,212);
+  strokeWeight(5);	
+	for (i=0; i<300; i++) {
+		stroke(map(i,0,20,75,241)%255,map(i,0,20,225,240)%255,map(i,0,20,240,102)%255);
+	
+		line(width+i,width+i+i,i*-(map(mouseX,0,windowWidth,-windowWidth/2,windowWidth/2)),0);
+		line(width+i,width+i+i+i,i*(map(mouseX,0,windowWidth,-windowWidth/2,windowWidth/2)),0);
+		
+		stroke(map(i,0,20,19,88)%255,map(i,0,20,121,56)%255,map(i,0,20,164,163)%255);
+		
+		line(-width+i,width+i+i,i*-(map(mouseX,0,windowWidth,-windowWidth/2,windowWidth/2)),0);
+		line(-width+i,width+i+i+i,i*(map(mouseX,0,windowWidth,-windowWidth/2,windowWidth/2)),0);
 
-	for (let i = 1; i < 100; i++) {
-		stroke(map(mouseX * mouseX, 0, width * height, 200, 255));
-
-		line(i * i + random(-0.1, 0.1), mouseX * 5 - 1000 + random(-0.1, 0.1), i + random(-0.1, 0.1), -mouseY * 100) + random(-0.1, 0.1);
-		line(i * i + random(-0.1, 0.1), mouseX * 5 - 1000 + random(-0.1, 0.1), i + random(-0.1, 0.1), mouseY + random(-0.1, 0.1));
-
-		line(i * i + random(-0.1, 0.1), mouseX - 300 + random(-0.1, 0.1), mouseY * 100 + random(-0.1, 0.1));
-
-
-		line(mouseX + random(-0.1, 0.1), -mouseY + random(-0.1, 0.1), -mouseX + (i * 3) % 5000 + random(-0.1, 0.1), -mouseX + i * 10 + random(-0.1, 0.1));
-
-		line(windowWidth - mouseX + random(-0.1, 0.1), -windowHeight - mouseY + random(-0.1, 0.1), -mouseX + (i * 3) % 5000 + random(-0.1, 0.1), -mouseX + i * 10 + random(-0.1, 0.1));
-
-	}
+}
+	
 }
